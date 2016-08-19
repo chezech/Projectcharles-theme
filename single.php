3f537280576b8802e937579bea4e8032c23b955a
chezech <chezech@gmail.com>
2016-08-19 18:59:35 -0400

    Add files via upload

--- single.php
+++ single.php
@@ -8,28 +8,22 @@
  */
 
 get_header(); ?>
+<div class="main">
+    <div class="container clearfix">
+        <div class="content">
+            <section>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		      </section>
+        </div><!------.content----------->
+		        <div class="sidebar">
+            <aside>
+			<?php get_sidebar(); ?>
+            </aside>
+        </div>
+    </div><!------.container----------->
+	</div><!-- .main-->
 
 <?php
-get_sidebar();
 get_footer();
