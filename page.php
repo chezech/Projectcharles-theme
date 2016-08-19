75790abc12b24cc48fdc6ec19b83e0e57b6e6285
chezech <chezech@gmail.com>
2016-08-19 18:55:56 -0400

    Add files via upload

--- page.php
+++ page.php
@@ -13,7 +13,34 @@
  */
 
 get_header(); ?>
+<div class="main">
+    <div class="container clearfix">
+        <div class="content">
+            <section>
 
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		      </section>
+        </div><!------.content----------->
+		<!-----------------display sidebar by the side of the main content------------->
+		        <div class="sidebar">
+            <aside>
+			<?php get_sidebar(); ?>
+            </aside>
+        </div>
+    </div><!------.container----------->
+	</div><!-- .main-->
 
 <?php
 get_sidebar();
