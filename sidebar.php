1968693c09f68a1ac9243ae778064118f94ecf44
chezech <chezech@gmail.com>
2016-08-19 18:58:28 -0400

    Add files via upload

--- sidebar.php
+++ sidebar.php
@@ -16,3 +16,23 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
+<aside>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-1') ) : ?>
+<?php endif; ?>
+</aside>
+<aside>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-2') ) : ?>
+<?php endif; ?>
+</aside>
+<aside>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-3') ) : ?>
+<?php endif; ?>
+</aside>
+<aside>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-4') ) : ?>
+<?php endif; ?>
+</aside>
+<aside>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-5') ) : ?>
+<?php endif; ?>
+</aside>
