ec93690537bc1f602c17353a30ba8c5e0511fd95
chezech <chezech@gmail.com>
2016-08-19 19:00:06 -0400

    Add files via upload

--- single.php
+++ single.php
@@ -13,7 +13,20 @@ get_header(); ?>
         <div class="content">
             <section>
 
+		<?php
+		while ( have_posts() ) : the_post();
 
+			get_template_part( 'template-parts/content', get_post_format() );
+
+			the_post_navigation();
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
 
 		      </section>
         </div><!------.content----------->
